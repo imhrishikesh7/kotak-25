ae2932a8af646be6bfee63776c7b18b9b78a6b65
Hrushikesh Pathak <hrushikeshpathak@Hrushikesh.local>
2025-08-01 18:44:06 +0530

    mobile issue fixed

--- src/pages/KPIInner.jsx
+++ src/pages/KPIInner.jsx
@@ -77,7 +77,7 @@ const KPIHome = () => {
 
     return (
         <div className=" overflow-hidden py-10 px-4 marginal">
-            <div className='w-fit mx-auto mb-6 text-center'>
+            <div className='w-fit md:block hidden mx-auto mb-6 text-center'>
                 <Reveal animation="slide-up">
                     <h3 className="text-2xl mb-2 font-bold text-[#ed1c25]">Key Performance Indicators</h3>
                 </Reveal>
@@ -95,8 +95,23 @@ const KPIHome = () => {
 
                 </Reveal>
             </div>
+            <div className='block md:hidden mb-6 mt-4'>
+
+                <h3 className="text-2xl mb-2 text-center font-bold text-[#ed1c25]">Key Performance Indicators</h3>
+
+                <div className='flex w-[50px] mb-2 mx-auto'>
+                    <div className='h-[2px] bg-[#ed1c25] w-1/2 mx-auto' />
+                    <div className='h-[2px] bg-[#013367] w-1/2 mx-auto' />
+                </div>
+
+                <h1 className="text-3xl text-center md:text-5xl pb-2 font-light text-transparent bg-gradient-to-r from-[#ed1c25] to-[#013367] bg-clip-text">Responsible growth, resilient performance</h1>
+
+                <p className='text-lg  leading-6 w-7xl'>
+                    All numbers are on a consolidated basis except where stated
+                </p>
+                </div>
             {/* Tabs */}
-            <div className="flex justify-center gap-6 mb-10">
+            <div className="flex md:flex-row flex-col justify-center gap-6 mb-10">
                 {Object.keys(chartImages).map((tab) => (
                     <button
                         key={tab}
